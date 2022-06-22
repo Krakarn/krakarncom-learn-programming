3280a38f41da50cb1bf3a131949f35bff1714c23
krakarn <linus.engelbrektsson@gmail.com>
2022-06-22 17:17:36 +0200

    Trying to fix broken site

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig({
-    base: "/krakarncom-learn-programming/",
     plugins: [
         monacoEditorPlugin(),
     ],
